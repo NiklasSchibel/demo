a08dc2d4d35e65a4bdae0b4dcfad418c7ea34774
NiklasSchibel <95278041+NiklasSchibel@users.noreply.github.com>
2022-03-12 18:07:38 +0100

    kap 2

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {getAllStudents} from "./client";
+import{useState,useEffect} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [students,setStudents] = useState([]);
+  const fetchStudents = () =>
+      getAllStudents()
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            setStudents(data);
+          })
+
+  useEffect(()=>{
+    console.log("component is mounted");
+    fetchStudents();
+  },[]);
+
+  if(students.length<=0){
+    return "no data";
+  }
+  return students.map((student,index)=>{
+    return <p key={index}>{student.id} {student.name}</p>
+  });
 }
 
 export default App;
