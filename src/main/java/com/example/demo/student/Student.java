a536d31abd191f43a06584f2a14634e4d55db5ed
NiklasSchibel <95278041+NiklasSchibel@users.noreply.github.com>
2022-04-05 17:38:41 +0200

    Kap4 (#2)
    * Layout Ant Design added
    
    * frontend cap done
    
    * up and running without database

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -2,13 +2,20 @@ package com.example.demo.student;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
     private Long id;
     private String name;
     private String email;
