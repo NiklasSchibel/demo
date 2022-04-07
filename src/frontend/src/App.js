ac24f4d77079558b626185bc8aac576d8d859e12
NiklasSchibel <95278041+NiklasSchibel@users.noreply.github.com>
2022-04-07 20:42:30 +0200

    Kap5 (#3)
    * new frontend feature
    
    * frontend ajusted
    
    * testing CICD

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,29 +1,74 @@
-import { useState, useEffect } from 'react'
-import { getAllStudents } from "./client";
+import {useState, useEffect} from 'react'
+import {deleteStudent, getAllStudents} from "./client";
 import {
     Layout,
     Menu,
     Breadcrumb,
-    Table, Spin, Empty,
+    Table,
+    Spin,
+    Empty,
+    Button, Badge, Tag, Avatar,
 } from 'antd';
+
 import {
     DesktopOutlined,
     PieChartOutlined,
     FileOutlined,
     TeamOutlined,
-    UserOutlined, LoadingOutlined,
+    UserOutlined,
+    LoadingOutlined,
+    PlusOutlined
 } from '@ant-design/icons';
+import StudentDrawerForm from "./StudentDrawerForm";
 
 import './App.css';
+import {errorNotification, successNotification} from "./Notification";
 
-const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
+const {Header, Content, Footer, Sider} = Layout;
+const {SubMenu} = Menu;
+
+const TheAvatar = ({name}) => {
+    let trim = name.trim();
+    if (trim.length === 0) {
+        return <Avatar icon={<UserOutlined/>}/>
+    }
+    const split = trim.split(" ");
+    if (split.length === 1) {
+        return <Avatar>{name.charAt(0)}</Avatar>
+    }
+    return <Avatar>
+        {`${name.charAt(0)}${name.charAt(name.length - 1)}`}
+    </Avatar>
+}
+
+
+const removeStudent = (studentId, callback) => {
+    deleteStudent(studentId).then(() => {
+        successNotification("Student deleted", `Student with ${studentId} was deleted`);
+        callback();
+    }).catch(err => {
+        err.response.json().then(res => {
+            console.log(res);
+            errorNotification(
+                "There was an issue",
+                `${res.message} [${res.status}] [${res.error}]`
+            )
+        });
+    })
+}
 
 const columns = [
     {
-        title: 'Key',
-        dataIndex: 'key',
-        key: 'key',
+        title: '',
+        dataIndex: 'avatar',
+        key: 'avatar',
+        render: (text, student) =>
+            <TheAvatar name={student.name}/>
+    },
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
     },
     {
         title: 'Name',
@@ -41,13 +86,14 @@ const columns = [
         key: 'gender',
     },
 ];
-const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
+const antIcon = <LoadingOutlined style={{fontSize: 24}} spin/>;
 
 function App() {
     const [students, setStudents] = useState([]);
-    const [collapsed, setCollapsed] = useState(false)
-    const [fetching,setFetching] = useState(true)
+    const [collapsed, setCollapsed] = useState(false);
+    const [fetching, setFetching] = useState(true);
+    const [showDrawer, setShowDrawer] = useState(false);
 
     const fetchStudents = () =>
         getAllStudents()
@@ -55,8 +101,16 @@ function App() {
             .then(data => {
                 console.log(data);
                 setStudents(data);
-                setFetching(false);
-            })
+            }).catch(err => {
+            console.log(err.response)
+            err.response.json().then(res => {
+                console.log(res);
+                errorNotification(
+                    "There was an issue",
+                    `${res.message} [${res.status}] [${res.error}]`
+                )
+            });
+        }).finally(() => setFetching(false))
 
     useEffect(() => {
         console.log("component is mounted");
@@ -65,60 +119,89 @@ function App() {
 
     const renderStudents = () => {
         if (fetching) {
-            return <Spin indicator={antIcon} />
+            return <Spin indicator={antIcon}/>
         }
         if (students.length <= 0) {
-            return <Empty />
+            return <>
+                <StudentDrawerForm
+                    showDrawer={showDrawer}
+                    setShowDrawer={setShowDrawer}
+                    fetchStudents={fetchStudents}
+                />
+                <Button
+                onClick={() => setShowDrawer(!showDrawer)}
+                type="primary" shape="round" icon={<PlusOutlined/>} size="small">
+                Add New Student
+                </Button>
+                <Empty/>
+            </>;
         }
-        return <Table
-            dataSource={students}
-            columns={columns}
-            bordered
-            title ={()=>"Students"}
-            pagination={{ pageSize: 50 }}
-            scroll={{ y: 240 }}
-            rowKey={(student)=>student.id}
-        />;
+        return <>
+            <StudentDrawerForm
+                showDrawer={showDrawer}
+                setShowDrawer={setShowDrawer}
+                fetchStudents={fetchStudents}
+            />
+            <Table
+                dataSource={students}
+                columns={columns}
+                bordered
+                title={() =>
+                    <>
+                    <Button
+                        onClick={() => setShowDrawer(!showDrawer)}
+                        type="primary" shape="round" icon={<PlusOutlined/>} size="small">
+                        Add New Student
+                    </Button>
+                        <Tag>Number of students</Tag>
+                        <Badge count={students.length} className="site-badge-count-4"/>
+                    </>
+                }
+                pagination={{pageSize: 50}}
+                scroll={{y: 500}}
+                rowKey={student => student.id}
+            />
+        </>
+
     }
 
-
-    return <Layout style={{ minHeight: '100vh' }}>
+    return <Layout style={{minHeight: '100vh'}}>
         <Sider collapsible collapsed={collapsed}
                onCollapse={setCollapsed}>
-            <div className="logo" />
+            <div className="logo"/>
             <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<PieChartOutlined />}>
+                <Menu.Item key="1" icon={<PieChartOutlined/>}>
                     Option 1
                 </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined />}>
+                <Menu.Item key="2" icon={<DesktopOutlined/>}>
                     Option 2
                 </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+                <SubMenu key="sub1" icon={<UserOutlined/>} title="User">
                     <Menu.Item key="3">Tom</Menu.Item>
                     <Menu.Item key="4">Bill</Menu.Item>
                     <Menu.Item key="5">Alex</Menu.Item>
                 </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+                <SubMenu key="sub2" icon={<TeamOutlined/>} title="Team">
                     <Menu.Item key="6">Team 1</Menu.Item>
                     <Menu.Item key="8">Team 2</Menu.Item>
                 </SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined />}>
+                <Menu.Item key="9" icon={<FileOutlined/>}>
                     Files
                 </Menu.Item>
             </Menu>
         </Sider>
         <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }} />
-            <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
+            <Header className="site-layout-background" style={{padding: 0}}/>
+            <Content style={{margin: '0 16px'}}>
+                <Breadcrumb style={{margin: '16px 0'}}>
                     <Breadcrumb.Item>User</Breadcrumb.Item>
                     <Breadcrumb.Item>Bill</Breadcrumb.Item>
                 </Breadcrumb>
-                <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                <div className="site-layout-background" style={{padding: 24, minHeight: 360}}>
                     {renderStudents()}
                 </div>
             </Content>
-            <Footer style={{ textAlign: 'center' }}>Amigoscode Demo</Footer>
+            <Footer style={{textAlign: 'center'}}>By Amigoscode</Footer>
         </Layout>
     </Layout>
 }
--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,30 +1,26 @@
 package com.example.demo.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(path="api/v1/students")
+@AllArgsConstructor
 public class StudentController {
+
+    private final StudentService studentService;
 
     @GetMapping
     public List<Student> getAllStudents(){
-        List<Student> students = Arrays.asList(
-                new Student(
-                        1L,
-                        "Jamila",
-                        "jamila@amigscode.edu",
-                        Gender.FEMALE),
-                new Student(
-                        2L,
-                        "Alex",
-                        "Alex@amigscode.edu",
-                        Gender.MALE)
-        );
-        return students;
+        return studentService.getAllStudents();
+    }
+
+    @PostMapping
+    public void addStudent(@Valid @RequestBody Student student){
+        studentService.addStudent(student);
     }
 }
