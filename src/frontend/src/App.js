e2eb08f295c8c5ac2709e37a9692d342289a50be
NiklasSchibel <95278041+NiklasSchibel@users.noreply.github.com>
2022-04-08 09:44:01 +0200

    Kap5 (#7)
    * new frontend feature
    
    * frontend ajusted
    
    * testing CICD
    
    * frontend adjusted
    
    * fronted adjusted
    
    * maven wrapper added
    
    * maven: git update-index --chmod=+x mvnw
    
    * pom adjusted
    
    * tag in docker compose file
    
    * frontend adjusted

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -201,7 +201,7 @@ function App() {
                     {renderStudents()}
                 </div>
             </Content>
-            <Footer style={{textAlign: 'center'}}>By Amigoscode</Footer>
+            <Footer style={{textAlign: 'center'}}>By</Footer>
         </Layout>
     </Layout>
 }
